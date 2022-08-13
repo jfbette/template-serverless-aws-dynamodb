d627d690394246c0c05b9263c3b8904d97ade2cd
Jean-Francois BETTE <jf.bette@gmail.com>
2022-08-13 17:14:18 +0200

    Solved Lambda dependencies upload pb

--- index.js
+++ index.js
@@ -2,32 +2,28 @@ const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const AWS = require('aws-sdk');
-const {postLogger, logRequest} = require('./middleware/logger');
-const dynamoose = require("dynamoose");
-var Users = require('./models/users.js');
-
-
-
-
+const {logRequest} = require('./middleware/logger');
+const { handlerError } = require('./utils/error');
 const userRouter = require('./routes/users');
+const dynamoose = require('dynamoose');
 
-const { DYNAMODB_TABLE, IS_OFFLINE } = process.env;
+const { IS_OFFLINE } = process.env;
 
-const dynamoDb = IS_OFFLINE === 'true' ?
-  dynamoose.aws.ddb.local(): "";
+ if (IS_OFFLINE === 'true') {
+  dynamoose.aws.ddb.local();
+ }
 
-  
 app.use(bodyParser.json({ strict: false }));
 
 // Log every request
 app.use(logRequest);
 
 // Handle uncaught errors
-app.use(postLogger);
+app.use((err, req, res, next) => {
+ handlerError(err, res);
+});
 
 // Import all routes
 app.use('/users', userRouter);
-//Swagger Configuration  
 
 module.exports.handler = serverless(app);
--- middleware/logger.js
+++ middleware/logger.js
@@ -1,18 +1,8 @@
 const logger = require("../utils/logger");
 
-function postLogger(err, req, res, next) {
-    if (err) {
-        logger.error(err);
-    }
-
-    res.status(500).json({error: 'Internal service error'});
-};
-
-// Will log every request 
 function logRequest(req, res, next) {
     logger.info(req.url);
     next();
   }
 
-module.exports = { postLogger, logRequest};
-    
+module.exports = { logRequest};
